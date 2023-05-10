bb168c4709e784925becd4fce4594f7827857d67
piyoko <piyoko.reina.224@gmail.com>
2023-05-10 07:24:49 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,25 @@
+@if (Auth::check())
+    {{-- 予定へのリンク --}}
+    <li><a class="link link-hover" href="#">予定</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- 一括予約へのリンク --}}
+    <li><a class="link link-hover" href="#">一括予約</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- 予約一覧へのリンク --}}
+    <li><a class="link link-hover" href="#">予約一覧</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- 出社指示へのリンク --}}
+    <li><a class="link link-hover" href="#">出社指示</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- 出社取り下げへのリンク --}}
+    <li><a class="link link-hover" href="#">出社取り下げ</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
+@else
+    {{-- ユーザ登録ページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('register') }}">Signup</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログインページへのリンク --}}
+    <li><a class="link link-hover" href="{{ route('login') }}">Login</a></li>
+@endif
