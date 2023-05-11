34fe5be7d689fbeeaa6d14a8128d85b32ada4973
piyoko <piyoko.reina.224@gmail.com>
2023-05-11 07:38:57 +0000

    loginpage

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -8,12 +8,14 @@
     {{-- 予約一覧へのリンク --}}
     <li><a class="link link-hover" href="#">予約一覧</a></li>
     <li class="divider lg:hidden"></li>
-    {{-- 出社指示へのリンク --}}
-    <li><a class="link link-hover" href="#">出社指示</a></li>
-    <li class="divider lg:hidden"></li>
-    {{-- 出社取り下げへのリンク --}}
-    <li><a class="link link-hover" href="#">出社取り下げ</a></li>
-    <li class="divider lg:hidden"></li>
+    @can('admin-higher')　{{-- 管理者に表示される --}}
+        {{-- 出社指示へのリンク --}}
+        <li><a class="link link-hover" href="#">出社指示</a></li>
+        <li class="divider lg:hidden"></li>
+        {{-- 出社取り下げへのリンク --}}
+        <li><a class="link link-hover" href="#">出社取り下げ</a></li>
+        <li class="divider lg:hidden"></li>
+    @endcan
     {{-- ログアウトへのリンク --}}
     <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
 @else
